0c4873ef7a2166e074da974cd52c7ffb0654fd6e
Stefan Bodner <stefan.bodner@hak-kitz.ac.at>
2022-10-03 16:41:02 +0200

    World Creation & MonoGame Start

--- JungleAdventure/Game1.cs
+++ JungleAdventure/Game1.cs
@@ -14,6 +14,26 @@ namespace JungleAdventure
         private int boxWidth = 32;
         private int boxHeight = 32;
 
+        #region Worlds
+        static int[,] world = new int[,] {
+            { 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1 },
+            { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+            { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
+            { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0 },
+            { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+            { 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1 }};
+
+        #endregion
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -52,14 +72,28 @@ namespace JungleAdventure
 
             spriteBatch.Begin();
 
-            spriteBatch.Draw(boxes, new Rectangle(10, 10, boxWidth, boxHeight), Color.AliceBlue);
-            spriteBatch.Draw(boxes, new Rectangle(10, 10, boxWidth, boxHeight), Color.AliceBlue);
+            WorldCreation();
            
             spriteBatch.End();
 
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+        }
+
+        public void WorldCreation()
+        {
+            for (int y = 0; y < world.GetLength(0); y++)
+            {
+                for (int x = 0; x < world.GetLength(1); x++)
+                {
+                    if (world[y, x] == 1) // normal block
+                    {
+                        Rectangle block = new Rectangle(x * boxWidth, y * boxHeight, boxWidth, boxHeight);
+                        spriteBatch.Draw(boxes, block, Color.AliceBlue);
+                    }
+                }
+            }
         }
     }
 }
