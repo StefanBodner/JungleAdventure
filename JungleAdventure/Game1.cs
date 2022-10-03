6ad4b46e8efe7bb085edc9e2a4143f53c7b9a416
Stefan Bodner <stefan.bodner@hak-kitz.ac.at>
2022-10-03 16:22:25 +0200

    Projektdateien hinzufügen.

---
+++ JungleAdventure/Game1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace JungleAdventure
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private Texture2D boxes;         
+        private int playerHeight = 32;
+        private int playerWidth = 32;
+        private int boxWidth = 32;
+        private int boxHeight = 32;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            boxes = Content.Load<Texture2D>("dirt");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(boxes, new Rectangle(10, 10, boxWidth, boxHeight), Color.AliceBlue);
+            spriteBatch.Draw(boxes, new Rectangle(10, 10, boxWidth, boxHeight), Color.AliceBlue);
+           
+            spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
